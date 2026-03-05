52126a05f035313d50910354b7bd62b1aeb4b184
Parker Arnett <362581936@GoTVDSB.ca>
2026-03-05 13:02:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Topic_8___Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+        }
+        public static void Part1()
+        {
+            List<string> names = new List<string> { "Trinity", "Neo", "Morpheus", "Smith" };
+            Console.WriteLine(names[1] + " is 'The One'.");
+            Console.WriteLine(names[3] + " is the bad guy.");
+            Console.WriteLine("There are " + names.Count + " names in my List.");
+            names[3] = "Agent Smith";
+            Console.WriteLine(names[3] + " is the bad guy.");
+            Console.ReadLine(); // Keeps program from closing
+
+
+
+
+        }
+    }
+   
+} 
