7a66a1020db7f13ae983d5f0ab3d0c38e343eb82
Parker Arnett <362581936@GoTVDSB.ca>
2026-03-10 12:30:57 -0400

    Done

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Part3();
+            Part5();
         }
         public static void Part1()
         {
@@ -56,7 +56,7 @@
         public static void Part3()
         {
             Random generator = new Random();
-            List<int> numbers = new List<int> { 3,9,27,81,243 };
+            List<int> numbers = new List<int> { 3, 9, 27, 81, 243 };
             Console.WriteLine(numbers[3]);
             Console.WriteLine(numbers[1] + numbers[3]);
             Console.WriteLine(numbers[1 + 3]);
@@ -68,15 +68,81 @@
         }
         public static void Part4()
         {
+            int biggerThanFive= 0;
             List<int> numbers = new List<int>(); // Creates an empty List
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine("Please enter a number: ");
-                numbers.Add(Convert.ToInt32(Console.ReadLine());
+                numbers.Add(Convert.ToInt32(Console.ReadLine()));
             }
-            Console.ReadLine(); // Keeps the program from ending
+            Console.WriteLine("Press ENTER to continue:");
 
+            Console.ReadLine(); // Keeps the program from ending
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.WriteLine("The largest number is " + numbers.Max());
+            Console.WriteLine("The smallest number is " + numbers.Min());
+            Console.WriteLine("The sum is " + numbers.Sum());
+            Console.WriteLine("The average is " + numbers.Average());
+            Console.WriteLine();
+            for (int i = numbers.Count - 1; i >= 0; i--) // We start at the last index (Length-1) and end at zero.
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Numbers larger than 5 are:");
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] > 5)
+                {
+                    Console.Write(numbers[i] + " ");
+                    biggerThanFive += 1;    // Counts the number of values bigger than 5 challenge
+                }
+            }
+            Console.WriteLine("There are "+biggerThanFive+ " numbers bigger than five.");    
+        }
+
+        public static void Part5()
+        {
+            Random generator = new Random();
+            List<int> numbers = new List<int>();
+            int zeroCount = 0;
+
+            for (int i = 0; i < 20; i++)
+                numbers.Add(generator.Next(41));
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.WriteLine("The average is " + numbers.Average());
+            Console.WriteLine("The largest number is " + numbers.Max());
+            Console.WriteLine("The smallest number is " + numbers.Min());
+
+            // Prints words for multipes of ten
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 10)
+                    Console.Write("Ten ");
+                else if (numbers[i] == 20)
+                    Console.Write("Twenty ");
+                else if (numbers[i] == 30)
+                    Console.Write("Thirty ");
+                else if (numbers[i] == 40)
+                    Console.Write("Fourty ");
+                else
+                    Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Double List");
+            for (int i = 0; i < numbers.Count; i++)
+                numbers[i] = numbers[i] * 2;
+            Console.WriteLine("Set values < 50 to zero");
+            for (int i = 0; i < numbers.Count; i++)
+                if (numbers[i] < 50)
+                    numbers[i] = 0;
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.WriteLine("Challenge - Count Zeroes");
+            for (int i = 0; i < numbers.Count; i++)
+                if (numbers[i] == 0)
+                    zeroCount += 1;
+            Console.WriteLine("There are " + zeroCount + " zeroes");
         }
     }
-   
-} 
+}
